eb94cfbd2b659394bebeca292b5e5986e0124d46
Ahmed Laajam <47797524+ache154@users.noreply.github.com>
2022-10-20 14:42:14 +0100

    Update ami_details.ps1

--- ami_details.ps1
+++ ami_details.ps1
@@ -1,2 +1,3 @@
 # Get the image details of a specific AMI
+# Following script will get the image details of the ami with ID: 017fecd1353bcc96e, which is amazon/ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220912
 aws ec2 describe-images --owners amazon --image-ids ami-017fecd1353bcc96e 
